20f41229e06a762c6849bfffc1800a49c93db1c4
Melissa <melbrng@gmail.com>
2015-09-21 11:04:52 -0600

    added outlets and actions for calculating beerText;additional WhiskeyViewController class

---
+++ Alcolator/Alcolator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Alcolator
+//
+//  Created by Melissa Boring on 9/21/15.
+//  Copyright © 2015 Bloc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
